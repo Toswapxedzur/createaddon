faed7c6f5b5d18dd14aa3fc185b21eba8ec665cf
Toswapxedzur <zhufengyuejohn@gmail.com>
2026-02-08 19:19:45 +0800

    The first tutorial on Registrate

---
+++ src/main/java/com/minecart/createaddon/CreateAddon.java
@@ -0,0 +1,71 @@
+package com.minecart.createaddon;
+
+import com.mojang.logging.LogUtils;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipModifier;
+import net.createmod.catnip.lang.FontHelper;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(CreateAddon.MODID)
+public class CreateAddon {
+    public static final String MODID = "createaddon";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MODID);
+
+    static {
+//        REGISTRATE.defaultCreativeTab((ResourceKey<CreativeModeTab>) null);
+        REGISTRATE.defaultCreativeTab(CreativeModeTabs.COMBAT);
+        REGISTRATE.setTooltipModifierFactory(item -> new ItemDescription.Modifier(item, FontHelper.Palette.STANDARD_CREATE)
+                .andThen(TooltipModifier.mapNull(KineticStats.create(item))));
+    }
+
+    public CreateAddon(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+        ModBlocks.register();
+        REGISTRATE.registerEventListeners(modEventBus);
+        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+
+    public static ResourceLocation modLoc(String path){
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
+    }
+}
---
+++ src/main/java/com/minecart/createaddon/ModBlocks.java
@@ -0,0 +1,54 @@
+package com.minecart.createaddon;
+
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllTags;
+import com.simibubi.create.foundation.data.AssetLookup;
+import com.simibubi.create.foundation.data.BlockStateGen;
+import com.simibubi.create.foundation.data.ModelGen;
+import com.simibubi.create.foundation.data.TagGen;
+import com.tterrag.registrate.Registrate;
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
+import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.MapColor;
+
+import static com.minecart.createaddon.CreateAddon.REGISTRATE;
+
+public class ModBlocks {
+    public static final BlockEntry<RotatedPillarBlock> JUKEBOX_ENCASED_SHAFT = REGISTRATE
+            .block("jukebox_encased_shaft", RotatedPillarBlock::new)
+            .initialProperties(() -> Blocks.JUKEBOX)
+            .properties(p -> p.mapColor(MapColor.PODZOL))
+            .properties(p -> p.sound(SoundType.WOOD))
+            .properties(p -> p.noOcclusion())
+//            .blockstate((c, p) -> p.simpleBlock(c.get()))
+            .blockstate(BlockStateGen.axisBlockProvider(true))
+            .transform(TagGen.pickaxeOnly())
+            .tag(BlockTags.MINEABLE_WITH_AXE)
+            .tag(AllTags.AllBlockTags.FAN_TRANSPARENT.tag)
+            .lang("Jukebox Encased Shaft")
+            .tag(AllTags.AllBlockTags.CASING.tag)
+            .loot((loot, block) -> loot.dropOther(block, Items.JUKEBOX))
+            .item()
+            .tab(CreativeModeTabs.BUILDING_BLOCKS)
+            .removeTab(CreativeModeTabs.COMBAT)
+//            .model(AssetLookup::customItemModel).build()
+            .transform(ModelGen.customItemModel())
+            .recipe((ctx, provider) -> {
+                ShapelessRecipeBuilder.shapeless(RecipeCategory.BUILDING_BLOCKS, ctx.get())
+                        .requires(Items.JUKEBOX).requires(AllBlocks.SHAFT.asItem())
+                        .unlockedBy("has_shaft", RegistrateRecipeProvider.has(AllBlocks.SHAFT.asItem()))
+                        .save(provider, CreateAddon.modLoc("jukebox_encased_shaft_using_shafts"));
+            })
+            .register();
+
+    public static void register() {
+    }
+}
